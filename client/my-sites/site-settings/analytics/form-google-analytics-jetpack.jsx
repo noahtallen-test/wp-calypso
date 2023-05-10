2e45ae5cd5106ad358592fcb16f40811e9f1d14e
Robert Franklin <42627630+robertf4@users.noreply.github.com>
2023-05-10 15:10:56 -0700

    Check for active prop on sites

--- client/my-sites/site-settings/analytics/form-google-analytics-jetpack.jsx
+++ client/my-sites/site-settings/analytics/form-google-analytics-jetpack.jsx
@@ -53,7 +53,7 @@ const GoogleAnalyticsJetpackForm = ( {
 	const analyticsSupportUrl = 'https://jetpack.com/support/google-analytics/';
 	const nudgeTitle = translate( 'Connect your site to Google Analytics' );
 	const wooCommercePlugin = find( sitePlugins, { slug: 'woocommerce' } );
-	const wooCommerceActive = wooCommercePlugin ? wooCommercePlugin.active : false;
+	const wooCommerceActive = wooCommercePlugin ? wooCommercePlugin.sites[ siteId ].active : false;
 
 	useEffect( () => {
 		if ( jetpackModuleActive ) {
